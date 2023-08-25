16e05ab8585c5741c9deab2c710e80dd7499d9b7
Brian Mendez <97647685+B1mendez@users.noreply.github.com>
2023-08-24 19:29:58 -0700

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -13,31 +13,35 @@ module alu(
 always_comb begin 
   rslt = 'b0;            
   sc_o = 'b0;    
+
+  case(alu_cmd)
+    3'b000: // ADD
+      {sc_o,rslt} = inA + inB + sc_i;
+	  
+    3'b001: // SLL (Shift Left Logical)
+      {sc_o,rslt} = {inA, sc_i};
+	  
+    3'b010: // SRL (Shift Right Logical)
+      {rslt,sc_o} = {sc_i,inA};
+	  
+    3'b011: // XOR (Bitwise XOR)
+      rslt = inA ^ inB;
+	  
+    3'b100: // AND (Bitwise AND)
+      rslt = inA & inB;
+	  
+    3'b101: // OR (Bitwise OR)
+      rslt = inA | inB;
+
+     3'b110: // Subtract (This might be redundant given the scope, but I'm leaving it for now)
+      {sc_o,rslt} = inA - inB + sc_i;
+		
+    3'b111: // pass A (Simply pass input A to output)
+      rslt = inA;
+  endcase
+  
   zero = !rslt;
   pari = ^rslt;
-  case(alu_cmd)
-    3'b000: // add 2 8-bit unsigned; automatically makes carry-out
-      {sc_o,rslt} = inA + inB + sc_i;
-	3'b001: // left_shift
-	  {sc_o,rslt} = {inA, sc_i};
-      /*begin
-		rslt[7:1] = ina[6:0];
-		rslt[0]   = sc_i;
-		sc_o      = ina[7];
-      end*/
-    3'b010: // right shift (alternative syntax -- works like left shift
-	  {rslt,sc_o} = {sc_i,inA};
-    3'b011: // bitwise XOR
-	  rslt = inA ^ inB;
-	3'b100: // bitwise AND (mask)
-	  rslt = inA & inB;
-	3'b101: // left rotate
-	  rslt = {inA[6:0],inA[7]};
-	3'b110: // subtract
-	  {sc_o,rslt} = inA - inB + sc_i;
-	3'b111: // pass A
-	  rslt = inA;
-  endcase
 end
    
 endmodule
