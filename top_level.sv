bc1ea3a942043be8c259d1c946fed727ffe88565
Brian Mendez <97647685+B1mendez@users.noreply.github.com>
2023-08-29 18:41:56 -0700

    Update top_level.sv

--- top_level.sv
+++ top_level.sv
@@ -3,32 +3,33 @@ module top_level(
   input        clk, reset, 
   output logic done);
   parameter D = 12,             // program counter width
-    A = 3;             		  // ALU command bit width
+    A = 3;             		  	  // ALU command bit width
   wire[D-1:0] target, 			  // jump 
               prog_ctr;
   wire        RegWrite;
   wire[7:0]   datA,datB,		  // from RegFile
-              muxB, 
-					rslt,				// result from ALU
+              muxB, 				  // Not being used for now
+				  rslt,				  // result from ALU
               immed,
 				  writeData;
 				  
   logic sc_in,   				  // shift/carry out from/to ALU
-   		pariQ,              	  // registered parity flag from ALU
-		zeroQ;                    // registered zero flag from ALU 
+   	  pariQ,              	  // registered parity flag from ALU
+		  zeroQ;                    // registered zero flag from ALU 
   wire  relj;                     // from control to PC; relative jump enable
   wire  pari,
         zero,
-		sc_clr,
-		sc_en,
+		  sc_clr,
+		  sc_en,
         MemWrite,
         ALUSrc;		              // immediate switch
   wire[A-1:0] alu_cmd;
   wire[8:0]   mach_code;          // machine code
-  wire[1:0] rd_addrA, rd_adrB;    // address pointers to reg_file CHANGING TO 2 BITS FOR SOURCE REGISTER
+  wire[2:0] rd_addrA, rd_addrB;  // address pointers to reg_file CHANGING TO 2 BITS FOR SOURCE REGISTER
+  wire[1:0] sel_cmd;					//selection command
 // fetch subassembly
   PC #(.D(D)) 					  // D sets program counter width
-     pc1 (.reset            ,
+     pc1 (.reset           ,
          .clk              ,
 		 .reljump_en (relj),
 		 .absjump_en (absj),
@@ -37,7 +38,7 @@ module top_level(
 
 // lookup table to facilitate jumps/branches
   PC_LUT #(.D(D))
-    pl1 (.addr  (mach_code[8:0]), //how_high
+    pl1 (.addr  (mach_code[5:0]), //how_high
          .target (target));   
 
 // contains machine code
@@ -51,8 +52,9 @@ module top_level(
   .ALUSrc   , 
   .RegWrite);
 
-  assign rd_addrA = mach_code[2:0];
-  assign rd_addrB = mach_code[5:3];
+  assign rd_addrA = mach_code[5:3];
+  assign rd_addrB = mach_code[2:0];
+  assign sel_cmd  = mach_code[1:0];
   assign alu_cmd  = mach_code[8:6];
 
   reg_file #(.pw(3)) rf1(.dat_in(writeData),	   // loads, most ops
@@ -60,12 +62,13 @@ module top_level(
               .wr_en   (RegWrite),
               .rd_addrA(rd_addrA),
               .rd_addrB(rd_addrB),
-              .wr_addr (rd_addrB),      // in place operation
+              .wr_addr (rd_addrA),      // in place operation
               .datA_out(datA),
               .datB_out(datB)); 
 
   alu alu1(.alu_cmd(alu_cmd),
-         .inA    (datA),
+		 .sel_cmd(sel_cmd),
+       .inA    (datA),
 		 .inB    (datB),
 		 .sc_i   (sc),   // output from sc register
 		 .rslt   (rslt),
