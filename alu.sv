41a576b265ec7c74cae74340e602beb181e1155b
Brian Mendez <97647685+B1mendez@users.noreply.github.com>
2023-08-30 14:45:02 -0700

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -1,19 +1,21 @@
 // combinational -- no clock
 // sample -- change as desired
 module alu(
-  input[2:0] alu_cmd,    // ALU instructions
+  input[2:0] alu_cmd,     // ALU instructions
   input[1:0] sel_cmd,
-  input[7:0] inA, inB,	 // 8-bit wide data path
-  input      sc_i,       // shift_carry in
+  input[7:0] inA, inB,	  // 8-bit wide data path
+  input      sc_i,        // shift_carry in
   output logic[7:0] rslt,
-  output logic sc_o,     // shift_carry out
-               pari,     // reduction XOR (output)
-			      zero      // NOR (output)
+  output logic sc_o,      // shift_carry out
+               pari,      // reduction XOR (output)
+			      zero,      // NOR (output)
+					br_logic   // BR 
 );
 
 always_comb begin 
   rslt = 'b0;            
-  sc_o = 'b0;    
+  sc_o = 'b0; 
+  br_logic = 'b0;
 
   case(alu_cmd)
     3'b000: // LDR STR & MOV
@@ -26,9 +28,15 @@ always_comb begin
 				rslt = inB;
 		endcase
 	  
-    3'b001: // BR  
-		rslt = inA;
+    3'b001: // CMP Compare function 
+	   begin
+			if (inA == inB)
+				br_logic = 'b1;
+			else
+				br_logic = 'b0;
 		
+			rslt = 1'b0;
+	   end
     3'b010: // OR (Bitwise OR)
       rslt = inA | inB;
 	  
@@ -45,7 +53,7 @@ always_comb begin
 		rslt = rslt << 1;   
 		
     3'b111: // br
-      rslt = inA;
+      rslt = 1'b0;
   endcase
   
   zero = !rslt;
