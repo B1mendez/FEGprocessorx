1a942b9fbbaeae2d5c183d6aa1ea111a53f523df
Brian Mendez <97647685+B1mendez@users.noreply.github.com>
2023-08-30 14:45:40 -0700

    Update top_level.sv

--- top_level.sv
+++ top_level.sv
@@ -1,96 +1,37 @@
-// sample top level design
-module top_level(
-  input        clk, reset, 
-  output logic done);
-  parameter D = 12,             // program counter width
-    A = 3;             		  	  // ALU command bit width
-  wire[D-1:0] target, 			  // jump 
-              prog_ctr;
-  wire        RegWrite;
-  wire[7:0]   datA,datB,		  // from RegFile
-              muxB, 				  // Not being used for now
-				  rslt,				  // result from ALU
-              immed,
-				  writeData;
-				  
-  logic sc_in,   				  // shift/carry out from/to ALU
-   	  pariQ,              	  // registered parity flag from ALU
-		  zeroQ;                    // registered zero flag from ALU 
-  wire  relj;                     // from control to PC; relative jump enable
-  wire  pari,
-        zero,
-		  sc_clr,
-		  sc_en,
-        MemWrite,
-        ALUSrc;		              // immediate switch
-  wire[A-1:0] alu_cmd;
-  wire[8:0]   mach_code;          // machine code
-  wire[2:0] rd_addrA, rd_addrB;  // address pointers to reg_file CHANGING TO 2 BITS FOR SOURCE REGISTER
-  wire[1:0] sel_cmd;					//selection command
-// fetch subassembly
-  PC #(.D(D)) 					  // D sets program counter width
-     pc1 (.reset           ,
-         .clk              ,
-		 .reljump_en (relj),
-		 .absjump_en (absj),
-		 .target     (target),
-		 .prog_ctr          );
-
-// lookup table to facilitate jumps/branches
-  PC_LUT #(.D(D))
-    pl1 (.addr  (mach_code[5:0]), //how_high
-         .target (target));   
-
-// contains machine code
-  instr_ROM ir1(.prog_ctr,
-               .mach_code);
-
 // control decoder
-  Control ctl1(.instr(mach_code[8:6]), 
-  .Branch  (relj)  , 
-  .MemWrite , 
-  .ALUSrc   , 
-  .RegWrite);
+module Control #(parameter opwidth = 3, mcodebits = 3)(
+  input [mcodebits-1:0] instr,    // subset of machine code (any width you need)
+  input 						br_logic,
+  output logic RegDst, Branch, 
+     MemtoReg, MemWrite, ALUSrc, RegWrite,
+  output logic[opwidth-1:0] ALUOp);	   // for up to 8 ALU operations
 
-  assign rd_addrA = mach_code[5:3];
-  assign rd_addrB = mach_code[2:0];
-  assign sel_cmd  = mach_code[1:0];
-  assign alu_cmd  = mach_code[8:6];
+always_comb begin
+// defaults
+  RegDst 	=   'b0;   // 1: not in place  just leave 0
+  Branch 	=   'b0;   // 1: branch (jump)
+  MemWrite  =	 'b0;   // 1: store to memory
+  ALUSrc 	=	 'b0;   // 1: immediate  0: second reg file output
+  RegWrite  =	 'b1;   // 0: for store or no op  1: most other operations 
+  MemtoReg  =	 'b0;   // 1: load -- route memory instead of ALU to reg_file data in
+  ALUOp	   =  'b111; // y = a+0;
+// sample values only -- use what you need
+case(instr)    			// override defaults with exceptions
+  'b000:  begin					      // store operation
+               MemWrite = 'b1;      // write to data mem
+               RegWrite = 'b0;      // typically don't also load reg_file
+			 end
+  'b001:  ALUOp      = 'b000;  // add:  y = a+b
+  'b010:  begin				    // load
+			   MemtoReg = 'b1;    // 
+          end
+  'b111:	 begin
+					if (br_logic)
+						Branch = 'b1;
+			 end
+// ...
+endcase
 
-  reg_file #(.pw(3)) rf1(.dat_in(writeData),	   // loads, most ops
-              .clk         ,
-              .wr_en   (RegWrite),
-              .rd_addrA(rd_addrA),
-              .rd_addrB(rd_addrB),
-              .wr_addr (rd_addrA),      // in place operation
-              .datA_out(datA),
-              .datB_out(datB)); 
-
-  alu alu1(.alu_cmd(alu_cmd),
-		 .sel_cmd(sel_cmd),
-       .inA    (datA),
-		 .inB    (datB),
-		 .sc_i   (sc),   // output from sc register
-		 .rslt   (rslt),
-		 .sc_o   (sc_o), // input to sc register
-		 .pari  );  
+end
 	
-  dat_mem dm1(.dat_in(datB)  ,  // from reg_file
-             .clk           ,
-			 .wr_en  (MemWrite), // stores
-			 .addr   (rslt), //store result data //was initially given as datA??? 
-             .dat_out(writeData));
-
-// registered flags from ALU
-  always_ff @(posedge clk) begin
-    pariQ <= pari;
-	zeroQ <= zero;
-    if(sc_clr)
-	  sc_in <= 'b0;
-    else if(sc_en)
-      sc_in <= sc_o;
-  end
-
-  assign done = prog_ctr == 128;
- 
 endmodule
