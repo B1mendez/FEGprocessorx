d5b56a4fde8bdc1e3ca4a1a560f53dcc17609bb5
Brian Mendez <97647685+B1mendez@users.noreply.github.com>
2023-09-01 18:49:30 -0700

    Update top_level.sv

--- top_level.sv
+++ top_level.sv
@@ -8,9 +8,9 @@ module top_level(
               prog_ctr;
   wire        RegWrite;
   wire[7:0]   datA,datB,		  // from RegFile
-              muxB, 				  // Not being used for now
+              muxA, 				  // mux for dataOut vs rslt
 				  rslt,				  // result from ALU
-				  writeData;
+				  writeData;		  // write for dataOut
 				  
   logic 		  sc_in,   				       // shift/carry out from/to ALU
 				  pariQ,              	    // registered parity flag from ALU
@@ -23,8 +23,8 @@ module top_level(
 		        sc_en,
               MemWrite,						// immediate switch
               ALUSrc,
+				  MemtoReg, 
 		        br_flag;		              
-  wire[A-1:0] alu_cmd;
   wire[8:0]   mach_code;          // machine code
   wire[2:0]   rd_addrA, rd_addrB;  // address pointers to reg_file CHANGING TO 2 BITS FOR SOURCE REGISTER
   wire[1:0]   immed;					//immed command
@@ -33,7 +33,6 @@ module top_level(
      pc1 (.reset           ,
        .clk              ,
 		 .reljump_en (relj),
-		 .absjump_en (absj),
 		 .target     (target),
 		 .prog_ctr          );
 
@@ -43,25 +42,25 @@ module top_level(
          .target (target));   
 
 // contains machine code
-  instr_ROM ir1(.prog_ctr,
-               .mach_code);
+  instr_ROM ir1(.prog_ctr (prog_ctr),
+               .mach_code (mach_code));
 
 // control decoder
-  Control ctl1(.instr	(mach_code[8:6]), 
-					.Branch  (relj)  	 , 
-					.MemWrite 		    , 
-					.ALUSrc   			 ,
-				   .br_logic			 ,
-					.RegWrite);
+  Control ctl1(.instr	 (mach_code[8:6]), 
+					.Branch   (relj), 
+					.MemWrite , 
+					.ALUSrc   ,
+				   .br_logic ,
+					.RegWrite ,
+					.MemtoReg ,
+					.ALUOp (mach_code[8:6]));
 					
 
   assign rd_addrA = mach_code[5:3];
   assign rd_addrB = mach_code[2:0];
   assign immed    = mach_code[1:0];
-  assign direct   = mach_code[2]; 
-  assign alu_cmd  = mach_code[8:6];
 
-  reg_file #(.pw(3)) rf1(.dat_in(writeData),	   // loads, most ops
+  reg_file #(.pw(3)) rf1(.dat_in(muxA),	   // loads, most ops
               .clk         ,
               .wr_en   (RegWrite),
               .rd_addrA(rd_addrA),
@@ -69,17 +68,16 @@ module top_level(
               .wr_addr (rd_addrA),      // in place operation
               .datA_out(datA),
               .datB_out(datB)); 
+				  
+  assign muxA = MemtoReg? writeData : rslt;
 
-  alu alu1(.alu_cmd(alu_cmd),
+  alu alu1(.alu_cmd(mach_code[8:6]),
 		 .immed (immed),
-		 .direct			,
+		 .direct	(mach_code[2]),
        .inA    (datA),
 		 .inB    (datB),
-		 .sc_i   (sc)  ,   // output from sc register
 		 .rslt   (rslt),
-		 .sc_o   (sc_o), // input to sc register
-		 .br_logic(br_flag), 
-		 .pari  );  
+		 .br_logic(br_flag));  
 	
   dat_mem dm1(.dat_in(datB)  ,  // from reg_file
              .clk           ,
@@ -89,14 +87,11 @@ module top_level(
 
 // registered flags from ALU
   always_ff @(posedge clk) begin
-    pariQ <= pari;
-	 zeroQ <= zero;
+//  pariQ <= pari;
+//	 zeroQ <= zero;
 	 br_logic <= br_flag;
 	 
-    if(sc_clr)
-	  sc_in <= 'b0;
-    else if(sc_en)
-      sc_in <= sc_o;
+ 
   end
 
   assign done = prog_ctr == 128;
