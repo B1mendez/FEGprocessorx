186cfbb39561e1c1d9146386e4075296253b4ecb
Jinshi He <89496409+SouKangC-school@users.noreply.github.com>
2023-09-04 15:09:46 -0700

    Update PC.sv

--- PC.sv
+++ PC.sv
@@ -4,7 +4,8 @@
 module PC #(parameter D=12)(
   input reset,					// synchronous reset
         clk,
-		  reljump_en,            // rel. jump enable
+		  jump_en,
+		  abs_jump,// rel. jump enable
   input       [D-1:0] target,	// how far/where to jump
   output logic[D-1:0] prog_ctr
 );
@@ -12,8 +13,13 @@ module PC #(parameter D=12)(
   always_ff @(posedge clk)
     if(reset)
 	  prog_ctr <= '0;
-	else if(reljump_en)
-	  prog_ctr <= prog_ctr + target;
+	else if(jump_en)
+	  begin
+	  if (abs_jump)
+			prog_ctr <= target;
+	  else	
+			prog_ctr <= prog_ctr + target;
+	  end
 	else
 	  prog_ctr <= prog_ctr + 'b1;
 
